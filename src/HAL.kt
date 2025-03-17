dfdc7898d09354dfefff7ae0a4511790912258ae
Rodrigo-fmd <a50451@alunos.isel.pt>
2025-03-17 15:53:57 +0000

    Initial commit

---
+++ src/HAL.kt
@@ -0,0 +1,34 @@
+import isel.leic.UsbPort
+
+object HAL {
+
+    private var output = 0
+
+    // Inicia o objeto
+    fun init() = UsbPort.write(output)
+
+    // Retorna 'true' se o bit definido pela mask esta como valor lógico '1' no UsbPort
+    fun isBit(mask : Int) : Boolean = mask.and(UsbPort.read()) == mask
+
+    // Retorna os valores dos bits representados por mask presentes no UsbPort
+    fun readBits(mask : Int) : Int = mask.and(UsbPort.read())
+
+    // Escreve nos bits representados por mask os valores dos bits correspondentes em value
+    fun writeBits(mask : Int, value: Int){
+       val input = UsbPort.read()
+       output = input.and(mask.inv()).or(value.and(mask))
+       UsbPort.write(output)
+    }
+
+    // Coloca os bits representados por mask no valor lógico '1'
+    fun setBits(mask : Int){
+        output = mask.or(output)
+        UsbPort.write(output)
+    }
+
+    // Coloca os bits representados por mask no valor lógico '0'
+    fun clrBits(mask : Int){
+        output = (mask.inv()).and(output)
+        UsbPort.write(output)
+    }
+}
---
+++ src/KBD.kt
@@ -0,0 +1,43 @@
+import isel.leic.UsbPort
+
+// Ler teclas. Funções retornam '0' ... '9', 'A' ... 'D', ’#’, ’*’ ou NONE.
+object KBD {
+
+    private const val NONE = 0
+    private const val DVAL_MASK = 0x10
+    private const val KEY_CODE_MASK = 0x0F
+    private const val ACK_MASK = 0x1
+    private const val INIT_VALUE = 0
+    private const val KEYS = "147*2580369#ABCD"
+
+    // Inicia a classe
+    fun init() = UsbPort.write(INIT_VALUE.and(ACK_MASK))
+
+
+
+    // Retorna de imediato a tecla premida ou NONE se nao ha tecla premida.
+    fun getKey(): Char {
+        if (HAL.isBit(DVAL_MASK)){
+            val key = HAL.readBits(KEY_CODE_MASK)
+            HAL.setBits(ACK_MASK)
+            return key.toChar()
+        }
+        else {
+            HAL.clrBits(ACK_MASK)
+            return NONE.toChar()
+        }
+    }
+
+    // Retorna a tecla premida, caso ocorra antes do 'timeout' (em milissegundos), ou NONE caso contrario.
+    fun waitKey(timeout: Long) : Char{
+        val start = System.currentTimeMillis()
+        while (System.currentTimeMillis() - start < timeout) {
+            val key = getKey()
+            if (key != NONE.toChar()){
+                return key
+            }
+        }
+        return NONE.toChar()
+    }
+}
+
