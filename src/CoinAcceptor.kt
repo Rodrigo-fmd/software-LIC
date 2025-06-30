7d52928a0ee74d283b8c34c1f132e120bddf3121
Rodrigo-fmd <rodrigo.duarte.6.rd@gmail.com>
2025-06-30 16:18:33 +0100

    ble

--- src/CoinAcceptor.kt
+++ src/CoinAcceptor.kt
@@ -6,22 +6,26 @@ object CoinAcceptor {
     private const val MASK_ID = 0x20
     private const val ENABLE_ACCEPT = 0x40
 
+    var previousCoinState = false
+
     fun checkAndAdd(currentAmount: Int): Int {
         var updatedAmount = currentAmount
 
-        if (HAL.isBit(MASK_COIN)) {
+        val coinDetected = HAL.isBit(MASK_COIN)
+
+        // Detecta borda de subida (de false para true)
+        if (coinDetected && !previousCoinState) {
             HAL.setBits(ENABLE_ACCEPT)
             Time.sleep(25)
 
             val coinValue = if (HAL.isBit(MASK_ID)) 4 else 2
             updatedAmount += coinValue
 
-            do {
-                Time.sleep(10)
-            }while (HAL.isBit(MASK_COIN))
-            // Wait until coin signal is cleared
             HAL.clrBits(ENABLE_ACCEPT)
         }
+
+        // Atualiza estado anterior
+        previousCoinState = coinDetected
 
         return updatedAmount
     }
